92a21d59806cdbe86e95a7e731846cacece6cbba
Nimira43 <n.rabone72@aol.com>
2025-03-21 10:17:04 +0000

    With the Patch decorator created method to update the user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post, Get, Patch, Param, Query, Delete } from '@nestjs/common'
 import { CreateUserDto } from './dtos/create-user.dto'
 import { UpdateUserDto } from './dtos/update-user.dto'
-import { UsersService } from './users.service';
+import { UsersService } from './users.service'
 
 @Controller('auth')
 export class UsersController {
@@ -25,6 +25,13 @@ export class UsersController {
   @Delete('/:id')
   removeUser(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id))
+  }
+
+  @Patch('/:id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body)
   }
 }
 
