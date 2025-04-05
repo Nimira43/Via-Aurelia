88079afea7db7f2d36a7ceb5b8555324e2d84bb1
Nimira43 <n.rabone72@aol.com>
2025-04-05 19:28:50 +0100

    leveraging  ClassSerializerInterceptor to control serialisation effectively

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -24,6 +24,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password)
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id))
