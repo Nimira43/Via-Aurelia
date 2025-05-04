bc5737c35d878e7b12a34ffd76330925cc45469d
Nimira43 <n.rabone72@aol.com>
2025-05-04 03:08:59 +0100

    Moved the Serialize decorator to be positioned around the whole UsersController class

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,7 +15,9 @@ import { UsersService } from './users.service'
 import { Serialize } from '../interceptors/serialize.interceptor'
 import { UserDto } from './dtos/user.dto'
 
+
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -24,7 +26,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password)
   }
 
-  @Serialize(UserDto)
+  
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     console.log('Handller is running')
