c82c88d4619d28cd8cfc383bbc9de4ca81fee608
Nimira43 <n.rabone72@aol.com>
2025-05-04 03:22:57 +0100

    Passing in ClassConstructor into Serialize

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -12,7 +12,7 @@ interface ClassConstructor {
   new (...args: any[]): {}
 }
 
-export function Serialize(dto: any) {
+export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto))
 }
 
