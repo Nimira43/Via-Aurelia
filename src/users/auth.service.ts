9418417beaef82e82287778dc2e63870a3f65a61
Nimira43 <n.rabone72@aol.com>
2025-05-04 05:44:28 +0100

    Console looging user - password has been salted and hashed successfully

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -20,6 +20,7 @@ export class AuthService {
     const hash = (await scrypt(password, salt, 32)) as Buffer
     const result = salt + '.' + hash.toString('hex')
     const user = await this.usersService.create(email, result)
+    console.log(user)
     return user
   }
 
