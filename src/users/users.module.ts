d1c417e419b2d5a548aeb1cb9c240efb549a81d8
Nimira43 <n.rabone72@aol.com>
2025-08-12 17:00:40 +0100

    Importing APP_INTERCEPTOR into users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { APP_INTERCEPTOR } from '@nestjs/core'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
