6b154bcc8befdac50704848c7951bd3afe4b2db2
Nimira43 <n.rabone72@aol.com>
2025-08-21 18:14:24 +0100

    Completed canActivate

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -2,6 +2,8 @@ import { CanActivate, ExecutionContext } from '@nestjs/common'
 
 export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
-    
+    const request = context.switchToHttp().getRequest()
+
+    return request.session.userId
   }
 }
