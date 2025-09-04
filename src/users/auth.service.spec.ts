f1ec4e65a44588804e6ebc1b7fd73bd00f6c706b
Nimira43 <n.rabone72@aol.com>
2025-09-04 21:46:35 +0100

    Testing setup - 9

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -6,4 +6,6 @@ it('can create an instance of auth service', async () => {
   const module = await Test.createTestingModule({
     providers: [AuthService]
   }).compile()
+
+  const service = module.get(AuthService)
 })
