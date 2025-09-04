529244c61b2aca1176e3a2e1197d9d815a92c456
Nimira43 <n.rabone72@aol.com>
2025-09-04 21:54:44 +0100

    fakeUsersService

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,8 +3,17 @@ import { AuthService } from './auth.service'
 import { UsersService } from './users.service'
 
 it('can create an instance of auth service', async () => {
+  const fakeUsersService = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>  Promise.resolve({
+      id: 1,
+      email,
+      password
+    }) 
+  }
+
   const module = await Test.createTestingModule({
-    providers: [AuthService]
+    providers: [AuthService],
   }).compile()
 
   const service = module.get(AuthService)
