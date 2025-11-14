125bb86d31c6b43d7099c43c2f58b7f4ec457251
Nimira43 <n.rabone72@aol.com>
2025-11-14 21:08:35 +0000

    Importing UsersService into users.controller.spec.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,4 @@
-import {
-  Body,
-  Controller,
-  Post,
-  Get,
-  Patch,
-  Param,
-  Query,
-  Delete,
-  NotFoundException,
-  Session,
-  UseGuards
-} from '@nestjs/common'
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, Session, UseGuards } from '@nestjs/common'
 import { CreateUserDto } from './dtos/create-user.dto'
 import { UpdateUserDto } from './dtos/update-user.dto'
 import { UsersService } from './users.service'
@@ -28,7 +16,7 @@ export class UsersController {
   constructor(
     private usersService: UsersService,
     private authService: AuthService
-  ) { }
+  ) {}
                         
   @Get('whoami')
   @UseGuards(AuthGuard)
