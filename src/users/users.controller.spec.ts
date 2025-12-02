3ce860474a96d5597f497121641de361e659514c
Nimira43 <n.rabone72@aol.com>
2025-12-02 01:02:32 +0000

    Error path test: overridden findOne to return null and asserted that NotFoundException is thrown

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -57,5 +57,8 @@ describe('UsersController', () => {
     expect(controller).toBeDefined()
   })
 
-  it('find')
+  it('findUser throws an error if user with given id is not found', async () => {
+    fakeUsersService.findOne = () => null
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException)
+  })
 })
